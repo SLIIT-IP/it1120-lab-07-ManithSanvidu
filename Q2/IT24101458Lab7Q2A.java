c5060d25a40b2aafdd3018e4b492f94be3f9e6c2
ManithSanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2024-09-20 11:28:38 +0530

    Commit  IT24101458Lab7Q2A.java to Q2

---
+++ Q2/IT24101458Lab7Q2A.java
@@ -0,0 +1,13 @@
+public class IT24101458Lab7Q2A{
+    public static void main(String[] args) {
+        int i,j;
+
+        for(i=0;i<4;i++){
+            for(j=0;j<5;j++){
+                System.out.print("$ ");
+            }
+            System.out.println();
+        }
+    }
+
+}
